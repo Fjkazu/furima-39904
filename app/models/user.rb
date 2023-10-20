44cddbe73e95a5b3e0ac958382a0c9053891e806
Fjkazu <fjkazu.designwork@gmail.com>
2023-10-20 19:07:21 +0900

    バリデーション全角漢字対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # has_many :buys
 
   validates :nickname, :last_name, :first_name, :kanalast_name, :kanafirst_name, :birthdate, presence: true
-  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶー－々〆〤　 ]+\z/ }
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :kanalast_name, :kanafirst_name, format: { with: /\A[ァ-ヶー－]+\z/ }
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX }
