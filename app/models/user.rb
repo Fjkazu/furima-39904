c9c6a97e99e4717ff6eec7f95819be3a41091ab7
Fjkazu <fjkazu.designwork@gmail.com>
2023-10-20 15:55:48 +0900

    ログイン、サインアップ機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # has_many :items
+  # has_many :buys
+
+  validates :nickname, :last_name, :first_name, :kanalast_name, :kanafirst_name, :birthdate, presence: true
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶー－々〆〤　 ]+\z/ }
+  validates :kanalast_name, :kanafirst_name, format: { with: /\A[ァ-ヶー－]+\z/ }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
