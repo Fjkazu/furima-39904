a200dc26758ebb41f2f1f089391bdf91ef983f18
Fjkazu <fjkazu.designwork@gmail.com>
2023-10-21 12:33:15 +0900

    ログイン状態のみ商品出品ページ遷移可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,13 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_index, only: [:new]
+
   def index
   end
-  
+
+  def new
+
+  end
 
 
 
@@ -9,6 +15,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :item_name, :overview, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :how_many_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
 end
