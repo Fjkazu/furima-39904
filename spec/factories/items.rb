30a0328b987a6f7aad50e54c49f0094e1a885c0e
Fjkazu <fjkazu.designwork@gmail.com>
2023-10-22 19:09:32 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,19 @@
 FactoryBot.define do
   factory :item do
-    item_name              { 'いじめっこ' }
-    overview                 { Faker::Internet.email }
-    category_id              { 'test1234' }
-    condition_id { password }
-    delivery_id             { 'ごうだ' }
-    prefecture_id            { 'たけし' }
-    how_many_day_id         { 'ゴウダ' }
-    price        { 'タケシ' }
-    image                 { Faker::Lorem.sentence }
+    item_name                { 'いじめっこ' }
+    overview                 { 'ジャイアンのことさ' }
+    category_id              { "2" }
+    condition_id             { "3" }
+    delivery_cost_id         { "2" }
+    prefecture_id            { "2" }
+    how_many_day_id          { "2" }
+    price                    { "500" }
+
+
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/akichi.png'), filename: 'akichi.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,21 +2,82 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.build(:user)
+    @item = FactoryBot.build(:item)
   end
 
-  describe 'ユーザー新規登録' do
-    context '新規登録できるとき' do
-      it 'ユーザー情報全て存在すれば登録できる' do
+  describe '商品情報入力' do
+    context '出品できるとき' do
+      it '９項目全て存在すれば登録できる' do
         expect(@item).to be_valid
       end
     end
 
-    context '新規登録できないとき' do
-      it 'nicknameが空では登録できない' do
-        @user.nickname = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    context '出品できないとき' do
+      it '商品名が空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it '商品説明が空では登録できない' do
+        @item.overview = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Overview can't be blank")
+      end
+      it 'カテゴリーが選択されていないと登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品状態が選択されていないと登録できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料負担が選択されていないと登録できない' do
+        @item.delivery_cost_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost can't be blank")
+      end
+      it '発送元地域が選択されていないと登録できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送日数が選択されていないと登録できない' do
+        @item.how_many_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("How many day can't be blank")
+      end
+
+      it '価格が空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格が300円未満では登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+      end
+      it '価格が10,000,000円以上では登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+      end
+      it '価格が全角では登録できない' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '価格が英字では登録できない' do
+        @item.price = 'abcd'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '画像が空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
     end
